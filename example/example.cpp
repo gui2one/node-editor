ea812fbaec9247363caeddb968167b5be7427ff9
gui2one <guillaume.rouault.fx@gmail.com>
2024-10-11 13:09:44 +0200

    added Serialize.h

--- example/example.cpp
+++ example/example.cpp
@@ -2,58 +2,9 @@
 #include "node_editor.h"
 #include "params.h"
 
-#include <yaml-cpp/yaml.h>
-
+#include "Serialize.h"
 using namespace NodeEditor;
 
-YAML::Emitter& operator << (YAML::Emitter& out, const glm::vec3& v) {
-	out << YAML::Flow;
-	out << YAML::BeginSeq << v.x << v.y << v.z << YAML::EndSeq;
-	return out;
-}
-YAML::Emitter& operator << (YAML::Emitter& out, const ImVec2& v) {
-	out << YAML::Flow;
-	out << YAML::BeginSeq << v.x << v.y << YAML::EndSeq;
-	return out;
-}
-template<typename T>
-YAML::Emitter& operator << (YAML::Emitter& out, Param<T>* param) {
-	out << YAML::Key <<param->name;
-  out << param->Eval();
-	return out;
-}
-YAML::Emitter& operator << (YAML::Emitter& out, const std::shared_ptr<ImGuiNode>& node) {
-	// auto op = static_cast<StringOperator *>(node.get());
-  out << node->title;
-  out << YAML::BeginMap;
-    out << YAML::Key << "position";
-    out << node->position;
-    out << YAML::Key << "params";
-      out << YAML::BeginMap;
-        for(auto param : node->m_Params) {
-          out << YAML::Key << param->name;
-          auto p_string = std::dynamic_pointer_cast<Param<std::string>>(param);
-          auto p_uint32_t = std::dynamic_pointer_cast<Param<uint32_t>>(param);
-
-          if( p_string) out << p_string->Eval();
-          else if( p_uint32_t) out << p_uint32_t->Eval();
-        }
-    
-      out << YAML::EndMap;
-  out << YAML::EndMap;
-	return out;
-}
-
-void serialize_nodes(std::vector<std::shared_ptr<ImGuiNode>> nodes) {
-  YAML::Emitter out;
-  for(auto node : nodes) {
-    // auto op = static_cast<StringOperator *>(node.get());
-    out << node;
-  }
-
-  std::cout << "Yaml output : \n"<< out.c_str() << std::endl;
-  
-}
 
 int main() {
 
