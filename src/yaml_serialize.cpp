ed5481c2ff0b45c55078c912dacc52afc5f54182
gui2one <guillaume.rouault.fx@gmail.com>
2024-10-16 12:08:09 +0200

    cleanup

--- src/yaml_serialize.cpp
+++ src/yaml_serialize.cpp
@@ -1,23 +1,9 @@
 #include "yaml_serialize.h"
 
-YAML::Emitter& operator << (YAML::Emitter& out, const glm::vec2& v) {
-    out << YAML::Flow;
-    out << YAML::BeginSeq << v.x << v.y << YAML::EndSeq;
-    return out;
-}
-
-YAML::Emitter& operator << (YAML::Emitter& out, const glm::vec3& v) {
-    out << YAML::Flow;
-    out << YAML::BeginSeq << v.x << v.y << v.z << YAML::EndSeq;
-    return out;
-}
-
 YAML::Emitter& operator << (YAML::Emitter& out, const std::shared_ptr<NodeEditor::ImGuiNode>& node) {
-
   out << node->YAMLSerialize();
   return out;
 }
-
 
 std::string serialize_nodes(std::vector<std::shared_ptr<NodeEditor::ImGuiNode>> nodes) {
 
@@ -33,38 +19,32 @@ std::vector<std::shared_ptr<NodeEditor::ImGuiNode>> deserialize_nodes(std::strin
   std::vector<std::shared_ptr<NodeEditor::ImGuiNode>> nodes;
   YAML::Node output = YAML::Load(yaml);
   for(auto node : output) {
-    // std::cout << YAML::Dump(node) << std::endl;
+
     std::string type_name = node["type"].as<std::string>();
-    // std::cout << "Deserializing type : " << type_name << std::endl;
+
     auto factory_node = NodeEditor::NodeFactoryRegistry::instance().create(type_name);
     factory_node->position = node["position"].as<ImVec2>();
     factory_node->title = node["title"].as<std::string>();
     factory_node->uuid = node["uuid"].as<std::string>();
 
-    int inc = 0;
-    for(auto p_node : node["params"]) {
+    for(size_t i= 0; i<node["params"].size(); i++) {
+      auto p_node = node["params"][i];
       std::string p_type_str = p_node["type"].as<std::string>();
+      auto param = factory_node->m_ParamLayout.items[i].param;
       
-      if(p_type_str == "std::string") {
-        NodeEditor::set_param_value<std::string>(factory_node->m_ParamLayout.items[inc].param, p_node["value"].as<std::string>());
-      }else if(p_type_str.find("std::basic_string") != std::string::npos) {
-        
-        NodeEditor::set_param_value<std::string>(factory_node->m_ParamLayout.items[inc].param, p_node["value"].as<std::string>());
+      if(p_type_str == "std::string" || p_type_str.find("std::basic_string") != std::string::npos) {
+        NodeEditor::set_param_value<std::string>(param, p_node["value"].as<std::string>());
       }else if(p_type_str == "unsigned int") {
-        NodeEditor::set_param_value<unsigned int>(factory_node->m_ParamLayout.items[inc].param, p_node["value"].as<unsigned int>());
-        
+        NodeEditor::set_param_value<unsigned int>(param, p_node["value"].as<unsigned int>());
       }else if(p_type_str == "bool") {
-        NodeEditor::set_param_value<bool>(factory_node->m_ParamLayout.items[inc].param, p_node["value"].as<bool>()); 
+        NodeEditor::set_param_value<bool>(param, p_node["value"].as<bool>()); 
       }else if(p_type_str.find("struct glm::vec<2,float,0>") != std::string::npos) {
-        NodeEditor::set_param_value<glm::vec2>(factory_node->m_ParamLayout.items[inc].param, p_node["value"].as<glm::vec2>()); 
+        NodeEditor::set_param_value<glm::vec2>(param, p_node["value"].as<glm::vec2>()); 
       }else if(p_type_str.find("struct glm::vec<3,float,0>") != std::string::npos) {
-        
-        NodeEditor::set_param_value<glm::vec3>(factory_node->m_ParamLayout.items[inc].param, p_node["value"].as<glm::vec3>()); 
-      }
-      inc++; 
+        NodeEditor::set_param_value<glm::vec3>(param, p_node["value"].as<glm::vec3>()); 
+      } 
     }
-
-
+    
     nodes.push_back(factory_node);
   }
 
