d4055646ecf7093a0e8a02006b0402a9374be694
gui2one <guillaume.rouault.fx@gmail.com>
2024-11-10 22:15:57 +0100

    cmake config

---
+++ dependencies.cmake
@@ -0,0 +1,62 @@
+include(FetchContent)   
+
+set(FETCHCONTENT_UPDATES_DISCONNECTED ON)
+set(FETCHCONTENT_QUIET FALSE)
+
+
+function(FetchDependencyAndMakeAvailable NAME URL TAG)
+  FetchContent_Declare(
+    ${NAME}
+    GIT_REPOSITORY ${URL}
+    GIT_TAG        ${TAG}
+    GIT_PROGRESS TRUE
+  )
+  FetchContent_MakeAvailable(${NAME})
+endfunction()
+
+# YAML-CPP
+set(YAML_CPP_BUILD_TOOLS OFF)
+set(BUILD_SHARED_LIBS OFF)
+FetchDependencyAndMakeAvailable(yaml-cpp https://github.com/jbeder/yaml-cpp da82fd982c260e7f335ce5acbceff24b270544d1) #master 10/11/2024
+
+# GLM
+FetchDependencyAndMakeAvailable(
+  glm
+  https://github.com/g-truc/glm
+  33b4a621a697a305bc3a7610d290677b96beb181
+)
+
+# GLFW
+set(GLFW_BUILD_SHARED_LIBRARY ON)
+FetchDependencyAndMakeAvailable(
+  glfw
+  https://github.com/glfw/glfw
+  7b6aead9fb88b3623e3b3725ebb42670cbe4c579
+)
+
+#IMGUI
+FetchDependencyAndMakeAvailable(
+  imgui
+  https://github.com/ocornut/imgui
+  v1.91.2-docking
+)
+
+set(imgui_sources 
+  ${CMAKE_SOURCE_DIR}/build/_deps/imgui-src/backends/imgui_impl_glfw.cpp
+  ${CMAKE_SOURCE_DIR}/build/_deps/imgui-src/backends/imgui_impl_opengl3.cpp
+  ${CMAKE_SOURCE_DIR}/build/_deps/imgui-src/misc/cpp/imgui_stdlib.cpp
+
+  ${CMAKE_SOURCE_DIR}/build/_deps/imgui-src/imgui.cpp
+  ${CMAKE_SOURCE_DIR}/build/_deps/imgui-src/imgui_demo.cpp
+  ${CMAKE_SOURCE_DIR}/build/_deps/imgui-src/imgui_draw.cpp
+  ${CMAKE_SOURCE_DIR}/build/_deps/imgui-src/imgui_widgets.cpp
+  ${CMAKE_SOURCE_DIR}/build/_deps/imgui-src/imgui_tables.cpp     
+)
+
+add_library(imgui STATIC ${imgui_sources})
+
+target_include_directories(imgui PUBLIC 
+  ${CMAKE_SOURCE_DIR}/_deps/imgui-src
+  ${CMAKE_SOURCE_DIR}/_deps/glfw-src/include
+)
+
